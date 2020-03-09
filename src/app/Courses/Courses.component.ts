081b4aca3d2725fa022e942abe372263b5ee5b2c
balajar87 <57900911+balajar87@users.noreply.github.com>
2020-03-09 19:02:44 +0530

    Commit

--- src/app/Courses/Courses.component.ts
+++ src/app/Courses/Courses.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface PeriodicElement {  
+  CourseID: number;
+  CourseName: string;
+  Duration: number;
+  Cost: string;
+  isedit: boolean;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { CourseID: 1, CourseName: 'Designing Course', Duration: 6, Cost: 'H', isedit: false },
+  { CourseID: 2, CourseName: 'Designing Course1', Duration: 10, Cost: 'H', isedit: false },
+  { CourseID: 3, CourseName: 'Designing Course2', Duration: 15, Cost: 'H', isedit: false },
+];
+
 @Component({
   selector: 'app-Courses',
   templateUrl: './Courses.component.html',
